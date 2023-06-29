ab10925a775c46491a51d1efc7e2ac3829b606b3
arpan <Rockarpan31@gmail.com>
2023-06-29 16:07:34 +0545

    ookokok

--- app/http/controllers/admin/orderController.js
+++ app/http/controllers/admin/orderController.js
@@ -2,18 +2,23 @@ const Order = require("../../../models/order");
 
 function orderController() {
   return {
-    index(req, res) {
-      Order.find({ status: { $ne: "completed" } }, null, {
-        sort: { createdAt: -1 },
-      })
-        .populate("customerId", "-password")
-        .exec((err, orders) => {
-          if (req.xhr) {
-            return res.json(orders);
-          } else {
-            return res.render("admin/orders");
-          }
-        });
+    async index(req, res) {
+      try {
+        const orders = await Order.find({ status: { $ne: "completed" } })
+          .sort({ createdAt: -1 })
+          .populate("customerId", "-password");
+
+        if (req.xhr) {
+          return res.json(orders);
+        } else {
+          return res.render("admin/orders", { orders });
+        }
+      } catch (error) {
+        // Handle the error
+        console.error(error);
+        // Render an error page or redirect
+        return res.render("error", { error: "Something went wrong" });
+      }
     },
   };
 }
--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import moment from "moment";
-import Noty from "noty";
+//import Noty from "noty";
 
-export function initAdmin(socket) {
+ function initAdmin() {
   const orderTableBody = document.querySelector("#orderTableBody");
   let orders = [];
   let markup;
@@ -41,7 +41,7 @@ export function initAdmin(socket) {
               <p>${order._id}</p>
               <div>${renderItems(order.items)}</div>
           </td>
-          <td class="border px-4 py-2">${order.customerId.name}</td>
+          <td class="border px-4 py-2">${order.customerId.name}</td> 
           <td class="border px-4 py-2">${order.address}</td>
           <td class="border px-4 py-2">
               <div class="inline-block relative w-64">
@@ -89,25 +89,13 @@ export function initAdmin(socket) {
           <td class="border px-4 py-2">
               ${moment(order.createdAt).format("hh:mm A")}
           </td>
-          <td class="border px-4 py-2">
-              ${order.paymentStatus ? "paid" : "Not paid"}
-          </td>
+         
       </tr>
   `;
       })
-      .join("");
+      .join("")
   }
 
-  socket.on("orderPlaced", (order) => {
-    new Noty({
-      type: "success",
-      timeout: 1000,
-      text: "New Order Placed",
-      progressBar: false,
-      layout: "bottomLeft",
-    }).show();
-    orders.unshift(order);
-    orderTableBody.innerHTML = "";
-    orderTableBody.innerHTML = generateMarkup(orders);
-  });
+
 }
+module.exports=initAdmin
