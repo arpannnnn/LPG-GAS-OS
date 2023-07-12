6129ba2fdb0d77f3894e4d6c7ffe2ea63229d4cd
arpan <Rockarpan31@gmail.com>
2023-07-12 12:47:00 +0545

    order status

---
+++ app/http/controllers/admin/statusController.js
@@ -0,0 +1,19 @@
+const Order = require('../../../models/order');
+
+function statusController() {
+  return {
+    update(req, res) {
+      Order.updateOne({ _id: req.body.orderId }, { status: req.body.status })
+        .then(() => {
+          res.redirect('/admin/orders');
+        })
+        .catch((err) => {
+          // Handle the error
+          console.error(err);
+          res.redirect('/admin/orders');
+        });
+    }
+  };
+}
+
+module.exports = statusController;
--- routes/web.js
+++ routes/web.js
@@ -5,8 +5,8 @@ const homeController = require("../app/http/controllers/homeController");
 const cartController = require("../app/http/controllers/customers/cartController");
 const orderController = require("../app/http/controllers/customers/orderController");
 
-const AdminOrderController = require("../app/http/controllers/admin/orderController");
-
+const adminOrderController = require("../app/http/controllers/admin/orderController");
+const statusController = require("../app/http/controllers/admin/statusController");
 //Middlewares
 
 const guest =require('../app/http/middlewares/guest')
@@ -40,7 +40,8 @@ app.get('/customer/orders',auth,orderController().index)
 
 
 //Admin routes
-app.get("/admin/orders",admin,AdminOrderController().index)
+app.get("/admin/orders",admin,adminOrderController().index)
+app.post("/admin/order/status",admin,statusController().update)
 
 
 
